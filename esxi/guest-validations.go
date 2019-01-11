1c7cdee036745c4199db4263c681e80032ee7bf7
ahitchcock71 <ashley.hitchcock@gbst.com>
2019-01-11 12:08:31 +1100

    included new Windows 2016 os type windows9Server64Guest

--- esxi/guest-validations.go
+++ esxi/guest-validations.go
@@ -207,6 +207,7 @@ func validateGuestOsType(guestos string) bool {
     windows9-64
     windows9
     windows9server-64
+	windows9Server64Guest
     windowshyperv
     winlonghorn-64
     winlonghorn
